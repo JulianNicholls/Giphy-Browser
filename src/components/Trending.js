20bbc5649b1eb4afbaa03536a1ce6df29280e83a
Julian Nicholls <juliannicholls29@gmail.com>
2019-02-07 21:52:34 +0000

    iConvert Trending page to Hooks

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,46 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { API_KEY } from '../api';
 
-export class Trending extends Component {
-  state = {
-    gifs: {},
-  };
+const Trending = () => {
+  const [gifs, setGifs] = useState({});
 
-  async componentDidMount() {
+  const loadTrending = async () => {
     try {
       const response = await fetch(
         `http://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
       );
       const json = await response.json();
 
-      this.setState({ gifs: json });
+      setGifs(json);
     } catch (err) {
       console.error(err);
     }
-  }
+  };
 
-  render() {
-    const { gifs } = this.state;
+  // A useEffect() function cannot be an async function, so the async function
+  // must be elsewhere and called with () => { ... }.
+  useEffect(() => {
+    loadTrending();
+  }, []);
 
-    return (
-      <div className="trending">
-        <h1>Trending GIFs</h1>
+  return (
+    <div className="trending">
+      <h1>Trending GIFs</h1>
 
-        {!gifs.data ? (
-          <h2>Loading...</h2>
-        ) : (
-          <div className="trending-list">
-            {gifs.data.map(image => (
-              <a href={image.images.original.url} key={image.id}>
-                <img src={image.images.fixed_height.url} alt={image.title} />
-              </a>
-            ))}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+      {!gifs.data ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div className="trending-list">
+          {gifs.data.map(image => (
+            <a href={image.images.original.url} key={image.id}>
+              <img src={image.images.fixed_height.url} alt={image.title} />
+            </a>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Trending;
