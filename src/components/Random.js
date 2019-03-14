b736201ba4292bf73066c61314641613e5117a5c
Julian Nicholls <juliannicholls29@gmail.com>
2019-03-14 18:37:59 +0000

    Use useRef to store createInterval handle.

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,36 +1,34 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useRef, useEffect, Fragment } from 'react';
 
 import { API_KEY } from '../api';
 
 const Random = () => {
   const [gif, setGif] = useState({});
-  const [handle, setHandle] = useState(0);
+  const handle = useRef(0);
 
-  // This must be called when handle changes, so that the cleanup knows the
-  // updated state of the slideshow handle and not just its initial value of 0.
   useEffect(() => {
     loadRandom();
 
     return () => {
-      if (handle) clearInterval(handle);
+      if (handle.current) clearInterval(handle.current);
     };
-  }, [handle]);
+  }, []);
 
   const loadRandom = async () => {
-    try {
-      const response = await fetch(
-        `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&rating=R`
-      );
-      const json = await response.json();
-
-      setGif(json);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+     try {
+       const response = await fetch(
+         `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&rating=R`
+       );
+       const json = await response.json();
+ 
+       setGif(json);
+     } catch (err) {
+       console.error(err);
+     }
+   };
 
   const slideshow = () => {
-    setHandle(setInterval(loadRandom, 5000));
+    handle.current = setInterval(loadRandom, 5000);
   };
 
   const image = gif.data;
@@ -45,10 +43,10 @@ const Random = () => {
             <img src={image.images.original.url} alt={image.title} />
           </div>
           <div className="button-holder">
-            <button disabled={handle !== 0} onClick={loadRandom}>
+            <button disabled={handle.current !== 0} onClick={loadRandom}>
               Another!
             </button>
-            <button disabled={handle !== 0} onClick={slideshow}>
+            <button disabled={handle.current !== 0} onClick={slideshow}>
               Slide show
             </button>
           </div>
